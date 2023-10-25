69d39e3af7066fce11ea9facfefc48f27a58c8a5
seungpoemz <132769706+seungpoemz@users.noreply.github.com>
2023-10-25 18:11:17 +0800

    Posttest_6

---
+++ POSTTEST6/src/views/add.php
@@ -0,0 +1,82 @@
+<?php
+    require "../actions/db_connect.php";
+    if (isset($_POST['tambah'])) {
+        $judul_film = $_POST['judul_film'];
+        $genre = $_POST['genre'];
+        $tgl_rilis = $_POST['tgl_rilis'];
+        $studio_produksi = $_POST['studio_produksi'];
+        $sinopsis = $_POST['sinopsis'];
+        $trailer = $_POST['trailer'];
+        $status_film = $_POST['status_film'];
+
+        $img = $_FILES['gambar']['name'];
+        $explode = explode('.', $img);
+        $ekstensi = strtolower(end($explode));
+        $gambar_baru = date("Y-m-d") . " $judul_film.$ekstensi";
+        $tmp = $_FILES['gambar']['tmp_name'];
+
+        
+        if(move_uploaded_file($tmp, '../assets/'.$gambar_baru)){
+        $result = mysqli_query($conn, "INSERT INTO film (judul_film, genre, tgl_rilis, studio_produksi, sinopsis, trailer, status_film, gambar) 
+        VALUES ('$judul_film', '$genre', '$tgl_rilis', '$studio_produksi', '$sinopsis', '$trailer', '$status_film', '$gambar_baru')");
+        // $result = mysqli_query($conn, "INSERT INTO film (id, judul_film, genre, tgl_rilis, studio_produksi, sinopsis, trailer, status_film, gambar) 
+        // VALUES ('', $judul_film', '$genre', '$tgl_rilis', '$studio_produksi', '$sinopsis', '$trailer', '$status_film', '$gambar_baru')");
+        echo "Gambar berhasil diunggah.";
+        if ($result) {
+            echo "
+            <script>
+                alert('Data berhasil ditambahkan!');
+                document.location.href = 'dashboard.php';
+            </script>";
+        } else {
+            echo "
+            <script>
+                alert('Data gagal ditambahkan!');
+                document.location.href = 'add.php';
+            </script>";
+        }
+    } else {
+        echo "Gagal mengupload gambar";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+    <link rel="stylesheet" media="screen and (min-width: 320px)" href="../styles/mobilep2.css">
+</head>
+<body>
+    <section class="add-data">
+        <div class="add-form-container">
+        <div class="sm-btn" id="sm-btn">
+            <img src="../assets/sun.png" id="icon">
+        </div>
+            <h1>Tambah Data</h1><hr><br>
+            <form action="" method="post" enctype="multipart/form-data">
+                <label for="judul_film">Judul Film</label>
+                <input type="text" name="judul_film" class="textfield">
+                <label for="genre">Genre Film</label>
+                <input type="text" name="genre" class="textfield">
+                <label for="tgl_rilis">Tanggal Rilis</label>
+                <input type="text" name="tgl_rilis" class="textfield">
+                <label for="studio_produksi">Studio Produksi</label>
+                <input type="text" name="studio_produksi" class="textfield">
+                <label for="sinopsis">Sinopsis Film</label>
+                <input type="text" name="sinopsis" class="textfield">
+                <label for="trailer">Trailer Film</label>
+                <input type="text" name="trailer" class="textfield" placeholder="Masukkan Kode Trailer Film">
+                <label for="status_film">Status Penayangan Film</label>
+                <input type="text" name="status_film" class="textfield">
+                <label for="">Upload Gambar</label>
+                <input type="file" name="gambar" id=""><br>
+                <input type="submit" name="tambah" value="Tambah Data" class="Login">
+            </form>
+        </div>
+    </section>
+</body>
+<script src="../scripts/script.js"></script>
+</html>
